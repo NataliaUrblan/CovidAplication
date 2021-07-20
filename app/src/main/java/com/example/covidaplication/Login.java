1020f363152cd8d96526293a1773198422bfbe0b
natalia <natalia.uribe-blanco@etu.univ-nantes.fr>
2021-07-21 01:02:08 +0200

    changes

--- app/src/main/java/com/example/covidaplication/Login.java
+++ app/src/main/java/com/example/covidaplication/Login.java
@@ -6,16 +6,29 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class Login extends AppCompatActivity {
-
+    TextView tv1;
+    EditText EdtMatricula ;
+    Button BtnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EdtMatricula=findViewById(R.id.EdtMatricula);
+        BtnIngresar=findViewById(R.id.BtnIngresar);
+
     }
+    public void BtnIngresar(View view){
+        Intent i = new Intent(this,UsuarioIngresado.class);
+        i.putExtra("MatriculaIngresada", EdtMatricula.getText().toString());
+        startActivity(i);
+    }
+
     public void BtnInvitado(View view){
         Intent invitado = new Intent(this,RegistroInvitado.class);
         startActivity(invitado);
--- app/src/main/java/com/example/covidaplication/RegistroEncuesta.java
+++ app/src/main/java/com/example/covidaplication/RegistroEncuesta.java
@@ -2,7 +2,9 @@ package com.example.covidaplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistroEncuesta extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class RegistroEncuesta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_encuesta);
+    }
+    public void BtnRegistrarFormulario(View view){
+        Intent formregistro = new Intent(this,AccesoQR.class);
+        startActivity(formregistro);
     }
 }
